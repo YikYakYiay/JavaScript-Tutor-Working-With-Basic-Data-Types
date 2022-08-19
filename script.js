2357093883cb7c9f232acf82615c10cb17deedaf
AverageCardGuy <69718523+YikYakYiay@users.noreply.github.com>
2022-08-19 15:42:32 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+//Strings
+var name = "Olivia,";
+var animal = "cat";
+var objective = "golden treasure!";
+var time = "Today,";
+
+//Int
+var months = 5;
+var weeks = 2;
+var days = 6;
+
+//Boolean
+var prophecy = true;
+
+//Array
+var animalSound = ["oinking", "meowing", "barking", "chirping"];
+
+//Story
+console.log("Hello my name is " + name + " and here is my " + animal + " named Kristen!");
+console.log("We have travelled the seven seas to find my grandma’s " + objective);
+console.log("Today, it seems like we have finally found it. After" + months + " grueling months, " + weeks + "long weeks, and " + days + "excruciating days, it’s seem like we have finally found the treasure that belonged to my beloving grandma.")
+console.log ("Kristen doesn’t stop meowing as she circles the treasure, gently" + animalSound[1] + "as I dig up and open the chest, revealing a golden trinket as well as an opulent golden necklace. The prophecy was " + prophecy + " all along...")
+
